91d11f85e2d4ca614f9b29e09455cfcfb8f5958c
Passer <2787683540@qq.com>
2024-01-15 22:01:46 +0800

    Day01 初识C语言 初识数据类型 初识变量、常量

---
+++ Day01_初识/Day01_初识/初识C语言、数据类型、变量常量.cpp
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//放在代码的第一行（scanf报错）
+
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("你好\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//字符类型
+//	char ch = 'a';
+//	//整型
+//	int age = 20;
+//	//短整型
+//	short num = 10;
+//	//long
+//	//long long
+//	//单精度浮点型
+//	float weight = 55.5;
+//	//双精度浮点型
+//	double d = 0.0;
+//
+//	printf("%d\n", 100);//打印一个整数 - %d
+//	//sizeof  - 关键字 - 操作符 - 计算类型或者变量所占空间的大小
+//
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//创建一个变量
+//	int age = 18;
+//	double weight = 53.5;
+//
+//	age = age + 1;
+//	printf("%d\n", age);
+//	
+//	weight = weight - 10;
+//	printf("%lf\n", weight);
+//
+//	return 0;
+//}
+
+////全局变量
+//int a = 10;
+//int main() 
+//{
+//	//局部变量
+//	int a = 20;
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+//写一个代码求两个数的和
+//scanf函数是输入函数
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d\n", sum);
+	return 0;
+}
