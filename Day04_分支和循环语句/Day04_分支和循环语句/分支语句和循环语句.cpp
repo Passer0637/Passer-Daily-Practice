6dec08208e325c9b5d4b910ec50ee0af737d26c5
Passer <2787683540@qq.com>
2024-01-18 21:54:57 +0800

    Day_04 分支语句：if语句 and switch语句 循环语句：while语句

---
+++ Day04_分支和循环语句/Day04_分支和循环语句/分支语句和循环语句.cpp
@@ -0,0 +1,138 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//	int age = 701;
+	//if (age >= 18)
+	//	printf("成年\n");
+	//if (age >= 18)
+	//	printf("成年\n");
+	//else 
+	//{
+	//	printf("未成年\n");
+	//	printf("不能谈恋爱\n");
+	//}
+	//if (age < 18)
+	//	printf("少年\n");
+	//else if (age >= 26 && age < 26)
+	//	printf("青年\n");
+	//else if (age >= 26 && age < 60)
+	//	printf("中年\n");
+	//else if (age >= 40 && age < 60)
+	//	printf("壮年\n");
+	//else if (age >= 60 && age < 100)
+	//	printf("老年\n");
+	//else
+	//	printf("老不死\n");
+//适当的使用{}可以使代码的逻辑更加清楚。
+//代码风格很重要
+//		int a = 0;
+//		int b = 2;
+//		if (a == 1)
+//		{
+//			if (b == 2)
+//			{
+//				printf("hehe\n");
+//			}
+//		}
+//		else
+//		{
+//			printf("haha\n");
+//		}
+	//int num = 15;
+	//if (num % 2 == 1)
+	//	printf("%d 是奇数\n", num);
+	//C++
+	//C99
+	//for (int i = 0; i < 100; i++)
+	//{
+	//	if (i % 2 == 1)
+	//		printf("%d ", i);
+	//}
+//	return 0;
+//}
+
+//int main() 
+//{
+	//int day = 0;
+	//scanf("%d", &day);
+	//switch (day)
+	//{
+	//case 1:
+	//	printf("星期一\n");
+	//	break;
+	//case 2:
+	//	printf("星期二\n");
+	//	break;
+	//case 3:
+	//	printf("星期三\n");
+	//	break;
+	//case 4:
+	//	printf("星期四\n");
+	//	break;
+	//case 5:
+	//	printf("星期五\n");
+	//	break;
+	//case 6:
+	//	printf("星期六\n");
+	//	break;
+	//case 7:
+	//	printf("星期七\n");
+	//	break;
+	//}
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//	case 6:	
+//	case 7:
+//		printf("休息日\n");
+//		break;//可加可不加，一般建议加上
+//	}
+//
+//	return 0;
+//}
+
+//int main() 
+//{
+	//int i = 1;
+	//while (i <= 10)
+	//{
+	//	printf("%d\n", i);
+	//	i++;
+	//}
+	//int i = 1;
+	//while (i <= 10)
+	//{
+	//	if (i == 5)
+	//		break;
+	//	printf("%d\n", i);
+	//	i++;
+	//}
+	//int i = 0;
+	//while (i < 10)
+	//{
+	//	i += 1;
+	//	if (i == 5)
+	//		continue;
+	//	printf("%d\n", i);
+	//}
+	//int ch = getchar();
+	//putchar(ch);//输出一个字符
+	//int ch = 0;
+	//CRTL + Z - getchar 就读取结束
+//	while ((ch = getchar()) != EOF)
+//		putchar(ch);
+//	return 0;
+//
+//	return 0;
+//}
