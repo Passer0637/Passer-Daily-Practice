86d9424280ff7618bd0e804996c787d664e36b28
Passer <2787683540@qq.com>
2024-01-21 02:39:22 +0800

    Day06_函数 库函数 自定义函数 函数的参数 函数的调用 函数的嵌套使用和链式法则 函数的声明和定义

---
+++ Day06_函数/Day06_函数/减法函数模块.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int Sub(int x, int y)
+{
+	return x - y;
+}
---
+++ Day06_函数/Day06_函数/加法函数模块.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//¼Ó·¨
+int Add(int x, int y)
+{
+	return x + y;
+}
