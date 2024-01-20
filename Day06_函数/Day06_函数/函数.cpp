dc2f8dd3a875f2d42950d88d06df8f09e8a7b3e5
Passer <2787683540@qq.com>
2024-01-20 23:52:59 +0800

    Day06

---
+++ Day06_函数/Day06_函数/函数.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	char arr1[20] = {0};
+//	char arr2[] = "hello world";
+//	strcpy(arr1, arr2);
+//	printf("%s",arr1);//打印arr1这个字符串 %s - 以字符串的格式打印
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char str[50];
+//	strcpy(str, "This is string.h library function");
+//	puts(str);
+//	memset(str, '$', 7);
+//	puts(str);
+//
+//	return(0);
+//}
+
+//int get_max(int x, int y)
+//{
+//	int z = 0;
+//	if (x > y)
+//	{
+//		z = x;
+//	}
+//	else
+//	{
+//		z = y;
+//	}
+//	return z;//返回z - 返回较大值
+//}
+//int main()
+//{
+//	int a = 5;
+//	int b = 12;
+//	int max = get_max(a, b);//函数的调用
+//	printf("max = %d\n", max);
+//
+//	return 0;
+//}
+
+int main()
+{
+
+
+	return 0;
+}
