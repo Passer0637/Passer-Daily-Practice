c03de7cdb7c0a2649069484e47350eb773af301c
Passer <2787683540@qq.com>
2024-01-24 20:33:11 +0800

    Day12_操作符 表达式求值 - 隐式类型转换：整型提升 算术转换：也算隐式类型转换 操作符属性

---
+++ Day12_操作符/Day12_操作符/操作符.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+	//整型提升
+	//char a = 3;
+	//char b = 127;
+	//char c = a + b;
+	//printf("%d\n", c);
+	//printf("%u\n", sizeof c);
+	//printf("%u\n", sizeof +c);
+	//printf("%u\n", sizeof -c);
+	//算术转换
+	//int d = 123;
+	//double e = 253.53;
+	//printf("%f\n",  d + e);
+	//操作符的属性
+	//int a = 4;
+	//int b = 5;
+	//int c = a + b * 7;//优先级决定操作顺序
+	//int c = a + b + 7;//优先级不起作用，结合性决定顺序
+	//return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int ret = (++i) + (++i) + (++i);//问题代码
+//	printf("%d\n", ret);
+//	return 0;
+//}
